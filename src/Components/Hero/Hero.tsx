c428dd6a99d819ac35e9e3b81ab5b572e4a56508
Oleksandr-Berd <alex.berd86@gmail.com>
2023-06-10 14:08:30 +0300

    made hero

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,17 +1,19 @@
 import * as SC from "./HeroStyled"
-import { ReactComponent as HeroSvg } from "assets/images/heroMobile.svg"
+import heroImage from "assets/images/heroMobile.png"
 
 const Hero: React.FC = () => (
     <SC.HeroContainer>
-        <SC.ImageContainer><HeroSvg /></SC.ImageContainer>
-        <div>
-            <h2>What your BMI result means</h2>
-            <p>A BMI range of 18.5 to 24.9 is considered
+        <SC.ImageContainer>
+            <SC.Image src={heroImage} alt="heroImage" />
+        </SC.ImageContainer>
+        <SC.TextContainer>
+            <SC.Title>What your BMI result means</SC.Title>
+            <SC.Text>A BMI range of 18.5 to 24.9 is considered
                 a 'healthy weight.' Maintaining a healthy weight may lower your chances of
                 experiencing health issues later on, such as obesity and type 2 diabetes.
                 Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables.
-                Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.</p>
-        </div>
+                Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.</SC.Text>
+        </SC.TextContainer>
     </SC.HeroContainer>
 )
 
